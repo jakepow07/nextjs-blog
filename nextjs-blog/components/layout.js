0b84272fe1bd97f88b457dee2c8ed4dae5e4f89d
jakepow07 <65188545+jakepow07@users.noreply.github.com>
2023-10-25 18:03:26 -0400

    uncapitalizing imports

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,8 +1,8 @@
 import styles from './layout.module.css'
 import Head from 'next/head';
-import Image from 'next/Image';
+import Image from 'next/image';
 import utilStyles from '../styles/utils.module.css'
-import Link from 'next/Link';
+import Link from 'next/link';
 
 
 const name = 'Your name';
